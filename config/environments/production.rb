427e7e2eec41b81e0ad00e846c2b56b8f3342f13
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-11-12 01:26:08 -0800

    fixed assest compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Maids::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+   config.assets.precompile += %w( glyphicons-halflings-regular.eot glyphicons-halflings-regular.svg glyphicons-halflings-regular.ttf glyphicons-halflings-regular.woff  )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
