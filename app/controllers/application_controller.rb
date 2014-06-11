4703ce94c548967fba785279bee7789f80bafa6a
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2014-06-11 10:56:02 -0700

    I added metatag for webmaster verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,36 +4,6 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery with: :exception
 
-  before_filter :prepare_for_mobile
   
   private
-
-  def mobile_device?
-    session.has_key?(:mobile) ? session[:mobile] : request.user_agent =~ /Mobile|webOS/
-  end
-
-  helper_method :mobile_device?
-
-
-  def device_type
-    request.env['mobvious.device_type']
-  end
-
-  helper_method :device_type
-
-
-
-
-  def prepare_for_mobile
-    if request.format == 'text/html'
-      case File.extname(URI.parse(request.fullpath).path)
-          when '.html'
-            session[:mobile] = false
-          when '.mobile'
-            session[:mobile] = true
-          end
-          request.format = :mobile if mobile_device?
-          request.format = :tablet if device_type == :tablet
-        end
-    end
 end
