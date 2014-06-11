b6257abec4a28ae98ce9fb971b810ec7abf37f74
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-06-11 15:06:36 -0700

    fixing mobile version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,36 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   helper :all
   protect_from_forgery with: :exception
-
+   before_filter :prepare_for_mobile
   
   private
+
+  def mobile_device?
+    session.has_key?(:mobile) ? session[:mobile] : request.user_agent =~ /Mobile|webOS/
+  end
+
+  helper_method :mobile_device?
+
+
+  def device_type
+    request.env['mobvious.device_type']
+  end
+
+  helper_method :device_type
+
+
+
+
+  def prepare_for_mobile
+    if request.format == 'text/html'
+      case File.extname(URI.parse(request.fullpath).path)
+          when '.html'
+            session[:mobile] = false
+          when '.mobile'
+            session[:mobile] = true
+          end
+          request.format = :mobile if mobile_device?
+          request.format = :tablet if device_type == :tablet
+      end
+  end
 end
