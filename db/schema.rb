ffc41b3db7d49e6f4947d2deec6ac4f212b9f422
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-06-19 23:42:12 -0700

    adjusted the position of the testimonial video

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131110192146) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "leads", force: true do |t|
     t.string   "email"
     t.string   "name"
